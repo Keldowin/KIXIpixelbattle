54bb01e61615d8f89893fc18d505b33d32bb5b46
KRIpto <84876763+Keldowin@users.noreply.github.com>
2022-05-15 01:33:06 +0300

    Update stat.php

--- stat.php
+++ stat.php
@@ -1,6 +1,6 @@
 <?php 
 require 'cfg.php';
-
+// Получаем статистику игроков и выводим 5 самых топовых по кликам
 $q = 'SELECT * FROM `users` ORDER BY `clicks` DESC LIMIT 6';
 $res = mysqli_query($link, $q);
 $TopPlayer = mysqli_fetch_all($res, MYSQLI_ASSOC);
