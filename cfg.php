9290e0b01020fed19c0516cb5c7ee77c6d316c6d
KRIpto <84876763+Keldowin@users.noreply.github.com>
2022-05-15 01:30:52 +0300

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,8 +1,9 @@
 <?php 
+// Кфг для базы данных и т.п
 $link = mysqli_connect('localhost', 'user', 'password', 'db');
 if (!$link) {
     exit('Ошибка бд');
 }
 $UserIP = $_SERVER['REMOTE_ADDR'];
-$open = 1; // 1 - закрыто, 0 - открыто
+$open = 1; // 1 - закрыто, 0 - открыто (Открытие или закрытие сайта)
 ?>
