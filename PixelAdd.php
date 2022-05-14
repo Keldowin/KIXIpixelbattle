46e99ece0eca93418204137d8680b4864183d94e
KRIpto <84876763+Keldowin@users.noreply.github.com>
2022-05-15 01:29:24 +0300

    Update PixelAdd.php
    Add description

--- PixelAdd.php
+++ PixelAdd.php
@@ -1,12 +1,15 @@
 <?php 
 require 'cfg.php';
 
+// Получаем и заносим все переменные из GET запроса
 $id = $_GET['id']+1;
 $color = $_GET['color'];
+// Если чё то пустое то сразу пишем в переменные такие числа
 if(empty($id) || empty($color)){
     $id = 1;
     $color = 'black';
 }
+// Выбераем пиксель из бд который указывали
 $q = 'SELECT `color` FROM `pixel` WHERE `id` = '.$id.'';
 $res = mysqli_query($link, $q);
 $data = mysqli_fetch_all($res, MYSQLI_ASSOC);
@@ -14,6 +17,7 @@ $data = mysqli_fetch_all($res, MYSQLI_ASSOC);
 $q = 'SELECT `ban` FROM `users` WHERE `ip` = "' . $UserIP . '"';
 $res = mysqli_query($link, $q);
 $player = mysqli_fetch_all($res, MYSQLI_ASSOC);
+// Система проверки забнен ли человек (Если да, то не добовляем пиксель)
 if(!empty($data) && $player[0]['ban'] != 1){
     $q = 'UPDATE `pixel` SET `color`="'.$color.'" WHERE `id` = '.$id.'';
     $res = mysqli_query($link, $q);
